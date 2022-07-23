163f65c44f233f5f1ff31a2ad1791d5f8cceb670
mkhisty <malharkhisty@gmail.com>
2022-07-23 12:54:02 -0400

    lil update

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 @app.route("/app", methods=['GET', 'POST'])
-def home():
+def main():
     return render_template("app.html")
 
 
 if __name__ == '__main__':
-    app.run(app, debug=True)
+    app.run(app)
