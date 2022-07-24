8828833d4eeaa9dce17480d53179b526ff28c0e8
mkhisty <malharkhisty@gmail.com>
2022-07-24 10:13:01 -0400

    Database manager half done

--- api/src/database_manager.py
+++ api/src/database_manager.py
@@ -1,9 +1,11 @@
 import sqlite3
-conn = sqlite3.connect("main.db")
+conn = sqlite3.connect("C:/Users/Owner/Desktop/SUPPLIERS.db")
 c=conn.cursor()
-c.execute("""CREATE TABLE database (
-    supplier real,
-    location integer,
-    services blob
-    )""")
+def get_suppliers(service,location):
+    c.execute("SELECT Name FROM Supplier WHERE ((Location = "+location+" )  AND (Services = "+service+"))" )
+    return [i[0] for i in c.fetchall()]
+def get_services(supplier):
+    c.execute("SELECT Services FROM Supplier WHERE Name ="+supplier)
+    return [i[0] for i in c.fetchall()]
+
 conn.commit()
