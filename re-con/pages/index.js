b310d8aa8b12dec7befd870ad8d4ad4deb1f0fb9
jeevsanp1 <100105187+jeevsanp1@users.noreply.github.com>
2022-07-24 23:19:54 -0400

    Added about

--- re-con/pages/index.js
+++ re-con/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import 'animate.css';
 
 export default function Home() {
   return (
@@ -16,7 +17,10 @@ export default function Home() {
         FIND YOUR <br /> RENEWABLE ENERGY FUTURE
         </h1>
         </div>
-
+        <div>
+          <h1 className={styles.about}>About Re-Con</h1>
+          <h3 className={styles.about_content}>Climate change is one of the biggest problems in recent history. With our software Renewable Energy Consultant (ReCon), corporations and individuals can input simple data and can find what energy source and provider is best for their location and budget. ReCon can also display tax rebates and other data to benefit the client. ReCon will have a profound impact in the future of the fight against climate change. Our solution uses software to bypass the traditionally costly consultancy which is one of the many factors why renewable resources are so inaccessible to the economically disadvantaged. ReCon will affect the lives of people from all walks of life and shift the tides against climate change.</h3>
+        </div>
       </main>
 
       {/* <footer className={styles.footer}>
